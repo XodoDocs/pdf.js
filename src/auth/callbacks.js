bb8bf0221f184304b32ebf891a1a6c9c95a207e0
Logan Bittner <lbittner@pdftron.com>
2019-08-21 12:54:23 -0700

    Basic implementation

---
+++ src/auth/callbacks.js
@@ -0,0 +1,8 @@
+
+export default ((module) => {
+  module.rC = (imgData, canvas, options, pageIdx) => {
+    canvas.getContext('2d').putImageData(imgData, 0, 0);
+    options['drawProgressive'](canvas);
+    options['drawComplete'](canvas, options, pageIdx);
+  };
+})(window);
